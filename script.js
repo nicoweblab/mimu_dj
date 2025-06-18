80702f8b63117006f79ea5db152a8c6653f54f05
nicoweblab <nicoweblab@gmail.com>
2025-06-18 14:49:37 -0300

    mitia lamejor del mundo

---
+++ script.js
@@ -0,0 +1,8 @@
+confirm ("¿deseas un poema para tu tia y futura madrina")                                             
+
+alert("estapaojina te da un poema para tu tia o futura madrina buenac embarasada")
+prompt("¿comocse llamqa tu tia o madrina?")  
+const boton = document.getElementById('te amo');
+buton . addeEventListener('click',() = > {  
+    window.location.href = 
+})
